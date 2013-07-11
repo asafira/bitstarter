daa0150f09c2adde8c0ec8f3e110f3e489012c34
Arthur Safira <Arthur.Safira@gmail.com>
2013-07-11 04:46:00 +0000

    Trying to get it to read from a file part 3

--- web.js
+++ web.js
@@ -1,13 +1,12 @@
 var express = require('express');
+var fs = require("fs");
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   fs.readFileSync('/index.html', function(err, data) {
-      if err throw err;
-      var buffer = new Buffer(16)
-      buffer.write(data)
-      response.send(buffer.toString);
+      if (err) throw err;
+      response.send(data.toString);
   });
   response.send('Hello World 2!');
 });
