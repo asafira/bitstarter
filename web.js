5475006772d6515b78c813dfbd17298cac226eb0
Arthur Safira <Arthur.Safira@gmail.com>
2013-07-11 05:09:16 +0000

    Trying to get it to read from a file part 6

--- web.js
+++ web.js
@@ -4,11 +4,8 @@ var fs = require("fs");
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  fs.readFileSync("index.html", function(err, data) {
-      if (err) throw err;
-      response.send(data.toString('binary'));
-  });
-  response.send('Hello World 2!');
+  var buffer = fs.readFileSync("index.html");
+  response.send(buffer.toString);
 });
 
 var port = process.env.PORT || 5000;
